357b6dc9d434f91b7164e3db960d9c66d8241678
Soumyo78 <soumyo78@yahoo.com>
2021-04-11 22:23:26 +0530

    Top Image Slide Carousel Animation Added

--- src/Components/Top_Image_Slide/script.js
+++ src/Components/Top_Image_Slide/script.js
@@ -1,5 +1,5 @@
 //Importing Files
-import React from 'react';
+import { useEffect } from 'react';
 import './style.css'
 
 //Importing Resources
@@ -11,55 +11,65 @@ import SlidePic5 from '../../Resources/Top_slide_pics/slide-pic-13.png';
 import SlidePic6 from '../../Resources/Top_slide_pics/slide-pic-14.png';
 
 let slideImgArr = [SlidePic1, SlidePic2, SlidePic3, SlidePic4, SlidePic5, SlidePic6];
-let slideIndexObj = {
-    currentIndex: 0
-};
 
-function autoSlideChange(){
-    setInterval(()=>{
-        slideIndexObj['currentIndex'] += 1;
-        if (slideIndexObj['currentIndex'] >= slideImgArr.length){
-            slideIndexObj['currentIndex'] = 0;
+const TopImageSlide = () =>{
+
+    const autoSlideChange = (locationDOM, imgArr) =>{
+        let offSet = 0;
+
+        const slideImg = (numberOfImg, locationDOM) =>{
+            let rate = 100/numberOfImg;
+            offSet = (offSet+rate);
+            locationDOM.style.transform = `translate(-${offSet}%, 0)`;
+            if (offSet >= 100){
+                offSet = 0;
+                locationDOM.style.transform = `translate(-${offSet}%, 0)`;
+            }
         }
-        showSlides(slideIndexObj['currentIndex']);
-    },4000)
-}
 
-autoSlideChange();
-
-function plusSlides(n) {
-    if (n){
-        slideIndexObj['currentIndex'] += 1;
-        if (slideIndexObj['currentIndex'] >= slideImgArr.length){
-            slideIndexObj['currentIndex'] = 0;
+        const slideImgBack = (numberOfImg, locationDOM) =>{
+            if(offSet > 0){
+                let rate = 100/numberOfImg;
+                offSet = (offSet-rate);
+                locationDOM.style.transform = `translate(-${offSet}%, 0)`;
+            }
         }
+
+        locationDOM.style.width = `${imgArr.length*100}%`;
+
+        imgArr.map((imgUrl, index) =>{
+            const imgContainer = document.createElement('div');
+            imgContainer.style.width = "100%";
+            imgContainer.style.marginLeft = "1px";
+            imgContainer.style.marginRight = "1px";
+            imgContainer.style.key = {index};
+
+            const slideImg = document.createElement('img');
+            slideImg.src = imgUrl;
+            slideImg.style.width = "100%";
+
+            imgContainer.appendChild(slideImg);
+            locationDOM.appendChild(imgContainer);
+        })
+
+        document.getElementById('next-btn').addEventListener('click', ()=>slideImg(imgArr.length, locationDOM));
+        document.getElementById('prev-btn').addEventListener('click', ()=>slideImgBack(imgArr.length, locationDOM));
+
+        setInterval(()=>slideImg(imgArr.length, locationDOM), 4000);
     }
 
-    if (!n){
-        slideIndexObj['currentIndex'] -= 1;
-        if (slideIndexObj['currentIndex'] <= 0){
-            slideIndexObj['currentIndex'] = (slideImgArr.length-1);
-        }
-    }
-    showSlides(slideIndexObj['currentIndex']);
-}
 
-function showSlides(n) {
-    document.getElementById('slide-img').setAttribute('src', slideImgArr[slideIndexObj["currentIndex"]]);
-}
+    useEffect(() => {
+        autoSlideChange(document.getElementById('slide-img-container'), slideImgArr)
+    }, [])
 
-class TopImageSlide extends React.Component{
-    render(){
-        return(
-            <div className="slide-img-button-container">
-                <div className="slide-img-container fade">
-                    <img id="slide-img" src={slideImgArr[slideIndexObj["currentIndex"]]} alt="" width="100%"/>
-                </div>
-                <a id="prev-btn" className="prev" onClick={()=>plusSlides(false)}>&#10094;</a>
-                <a id="next-btn" className="next" onClick={()=>plusSlides(true)}>&#10095;</a>
-            </div>
-        )
-    }
+    return(
+        <div className="slide-img-button-container">
+            <div className="slide-img-container" id="slide-img-container"></div>
+            <a id="prev-btn" className="prev">&#10094;</a>
+            <a id="next-btn" className="next">&#10095;</a>
+        </div>
+    )
 }
 
 export default TopImageSlide;
